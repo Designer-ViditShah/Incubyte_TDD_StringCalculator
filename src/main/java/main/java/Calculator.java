6d7bbcdbb1110232f52e88e499d0d10e7f992f85
Vidit <201911026@daiict.ac.in>
2021-03-18 19:27:55 +0530

    Fail Phase Question 1 Commit

--- src/main/java/main/java/Calculator.java
+++ src/main/java/main/java/Calculator.java
@@ -2,11 +2,19 @@ package main.java;
 
 public class Calculator {
     public static int Add(String inputString) {
-        if(inputString.isEmpty()){
+        if(checkIfStringEmpty(inputString)){
             return 0;
         }
-        else{
+        if(inputString.length() == 1){
             return Integer.parseInt(inputString);
         }
+        else{
+
+            return 0;
+        }
+    }
+
+    private static boolean checkIfStringEmpty(String inputString) {
+        return inputString.isEmpty();
     }
 }
--- src/main/java/main/java/TestStrngCalculator.java
+++ src/main/java/main/java/TestStrngCalculator.java
@@ -8,4 +8,21 @@ public class TestStrngCalculator {
     public void returnsIfZeroLengthStringFound(){
         assertEquals(Calculator.Add(""),0);
     }
+
+    @Test
+    public void returnsIfOneLengthStringFound(){
+        assertEquals(Calculator.Add("1"),1);
+    }
+
+
+    @Test
+    public void returnsSumOfTwoNumbersCommaSeperated(){
+        assertEquals(Calculator.Add("1,2"),3);
+    }
+
+    @Test
+    public void returnsSumOfTwoNumbersNewLineSeperated(){
+        assertEquals(Calculator.Add("1\n2"),3);
+    }
+
 }
