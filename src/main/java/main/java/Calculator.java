4a705671b2cdc5e5876bd3e525863d023fb9ddac
Vidit <201911026@daiict.ac.in>
2021-03-18 21:48:40 +0530

    Question 1 to 4 succesfully completed

--- src/main/java/main/java/Calculator.java
+++ src/main/java/main/java/Calculator.java
@@ -1,7 +1,11 @@
 package main.java;
 
 public class Calculator {
+    private static String deliminators = ",|\n";
+    private static int countAddMethodCalls = 0;
     public static int Add(String inputString) {
+        countAddMethodCalls++;
+//        GetCalledCount();
         if(checkIfStringEmpty(inputString)){
             return 0;
         }
@@ -9,20 +13,45 @@ public class Calculator {
             return Integer.parseInt(inputString);
         }
         else{
-            String[] numberSplit = inputString.split(",|\n");
+            if(inputString.startsWith("//")){
+                deliminators = Character.toString(inputString.charAt(2));
+                String customDelimiterStringNumbers = inputString.substring(4);
+                return AddHelper(customDelimiterStringNumbers, deliminators);
+            }
+            String[] numberSplit = inputString.split(deliminators);
             int sum = 0;
-            for(String addMe: numberSplit){
-                sum += convertStringToInteger(addMe);
+            for (String addMe : numberSplit) {
+                sum += convertStringToInteger(addMe.trim());
             }
             return sum;
         }
     }
 
+    private static int GetCalledCount() {
+        return countAddMethodCalls;
+    }
+
+    private static int AddHelper(String customDelimiterStringNumbers, String delimiters) {
+        int ans = 0;
+        String[] arrayString = customDelimiterStringNumbers.split(delimiters);
+        for(String number : arrayString){
+            ans += Integer.parseInt(number);
+        }
+        return ans;
+    }
+
     private static int convertStringToInteger(String convertMeToInt) {
+        if(convertMeToInt.isEmpty()){
+            return 0;
+        }
         return Integer.parseInt(convertMeToInt);
     }
 
     private static boolean checkIfStringEmpty(String inputString) {
         return inputString.isEmpty();
+    }
+
+    class customExceptionNegative extends Exception{
+
     }
 }
--- src/main/java/main/java/TestStrngCalculator.java
+++ src/main/java/main/java/TestStrngCalculator.java
@@ -1,6 +1,10 @@
 package main.java;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestStrngCalculator {
 
@@ -14,7 +18,6 @@ public class TestStrngCalculator {
         assertEquals(Calculator.Add("1"),1);
     }
 
-
     @Test
     public void returnsSumOfTwoNumbersCommaSeperated(){
         assertEquals(Calculator.Add("1,2"),3);
@@ -25,4 +28,17 @@ public class TestStrngCalculator {
         assertEquals(Calculator.Add("1\n2"),3);
     }
 
+    @Test
+    public void returnsSumOfMultipleNumbersWhichHasCustomDelimiterSeperated(){
+        assertEquals(Calculator.Add("//;\n1;2"),3);
+    }
+
+    @Test
+    public void forNegativeNumberThrowException() throws Exception{
+        negativeValueException();
+    }
+
+    private static void negativeValueException() throws IOException{
+        Calculator.Add("-1");
+    }
 }
