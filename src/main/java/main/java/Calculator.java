7fc6e6c6cd7c986164da73ba59d6356f3ddb772b
Vidit <201911026@daiict.ac.in>
2021-03-19 21:29:55 +0530

    Fail for sum of single delimiter

--- src/main/java/main/java/Calculator.java
+++ src/main/java/main/java/Calculator.java
@@ -1,59 +1,90 @@
 package main.java;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
 public class Calculator {
-    private static String deliminators = ",|\n";
+/*
+countAddMethodCalls -> used to count number of times Add() function is called
+deliminators        -> used as basic delimiter in case of string does not start with //
+START_INDEX_OFFSET  -> In case the string starts with //[ so custom delimiter will always start from 3 index
+startIndex          -> it gets updated for each [delimiter] in the string
+Pattern and Matcher helps to deal with the escape and the dangling characters that are used in the regular expression
+Few dangling characters are * [ \ etc.
+ */
     private static int countAddMethodCalls = 0;
-    public static int Add(String inputString) {
+    private static String deliminators = ",|\\n";
+    public static final int START_INDEX_OFFSET = 3;
+    private static Pattern customDelimitersValidator = Pattern.compile("//(\\[(\\D+)])+\n.*");
+    private static Matcher customDelimitersMatcher;
+    private static int startIndex;
+/*
+    Add function is called with string as an argument
+    Since incorrect argument's have been already dealt as mentioned in the question
+    Add function will throw an exception only in case of negative values
+    Case for empty string
+    Case for length one string
+    Case for 2 number separated with delimiter like , and \n
+    Case to ignore if the number is greater than 1000
+    Case for custom delimiter
+        -> it may start with [
+            = find the string after \n and save it to some memory
+            = find the string after // and before \n and save it to memory with name delimiterString
+            = In delimiterString
+                ~ replace the first [ with empty string
+                ~ replace all other [ with | symbol if it exits - this will take care for multiple delimiters in regex
+                ~ replace all ] with empty string
+                ~ using the Pattern compile We will compile the updated string and add each delimiter to deliminators
+
+        -> it has only one custom delimiter and does not start with [
+            = find the delimiter at the 2nd index starting from 0
+            = split the string after \n based on the delimiter found
+ */
+    public static int Add(String inputString) throws Exception {
         countAddMethodCalls++;
-//        GetCalledCount();
         if(checkIfStringEmpty(inputString)){
             return 0;
         }
         if(inputString.length() == 1){
-            return Integer.parseInt(inputString);
+            return convertStringToInteger(inputString);
         }
         else{
-            if(inputString.startsWith("//")){
-                deliminators = Character.toString(inputString.charAt(2));
-                String customDelimiterStringNumbers = inputString.substring(4);
-                return AddHelper(customDelimiterStringNumbers, deliminators);
-            }
-            String[] numberSplit = inputString.split(deliminators);
-            int sum = 0;
-            for (String addMe : numberSplit) {
-                if(convertStringToInteger(addMe)<=1000) {
-                    sum += convertStringToInteger(addMe);
-                }
-            }
-            return sum;
+            return AddHelper(inputString,deliminators);
         }
-    }
-
-    private static int GetCalledCount() {
-        return countAddMethodCalls;
-    }
-
-    private static int AddHelper(String customDelimiterStringNumbers, String delimiters) {
-        int ans = 0;
-        String[] arrayString = customDelimiterStringNumbers.split(delimiters);
-        for(String number : arrayString){
-            ans += Integer.parseInt(number);
-        }
-        return ans;
-    }
-
-    private static int convertStringToInteger(String convertMeToInt) {
-        if(convertMeToInt.isEmpty()){
-            return 0;
-        }
-        return Integer.parseInt(convertMeToInt);
     }
 
     private static boolean checkIfStringEmpty(String inputString) {
         return inputString.isEmpty();
     }
-
-    class customExceptionNegative extends Exception{
-
+/*
+    convertStringToInteger method simply converts the string value to integer value
+    note: testcase have been made such that the string will only contain "0" to "9" in any order
+ */
+    private static int convertStringToInteger(String convertMeToInt) {
+        return Integer.parseInt(convertMeToInt);
     }
+    private static int AddHelper(String customDelimiterStringNumbers, String delimiters) throws Exception {
+        int ans = 0;
+        String listOfNegativeNumbers = "";
+        if(delimiters.equals(",|\\n"))
+            delimiters = ",|\n";
+        String[] arrayString = customDelimiterStringNumbers.split(delimiters);
+        for(String number : arrayString){
+            if(convertStringToInteger(number)<=1000 && convertStringToInteger(number) >= 0) {
+                ans += Integer.parseInt(number);
+            }else if (convertStringToInteger(number)<0){
+                listOfNegativeNumbers += number + " ";
+            }
+        }
+        if(listOfNegativeNumbers.length()>0){
+            throw new IllegalArgumentException("negative not allowed "+listOfNegativeNumbers);
+        }
+        return ans;
+    }
+
+
 }
--- src/main/java/main/java/TestStrngCalculator.java
+++ src/main/java/main/java/TestStrngCalculator.java
@@ -1,42 +1,41 @@
 package main.java;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import java.util.Calendar;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TestStrngCalculator {
 
     @Test
-    public void returnsIfZeroLengthStringFound(){
+    public void returnsIfZeroLengthStringFound() throws Exception {
         assertEquals(Calculator.Add(""),0);
     }
 
     @Test
-    public void returnsIfOneLengthStringFound(){
+    public void returnsIfOneLengthStringFound() throws Exception {
         assertEquals(Calculator.Add("1"),1);
     }
 
     @Test
-    public void returnsSumOfTwoNumbersCommaSeperated(){
+    public void returnsSumOfTwoNumbersCommaSeperated() throws Exception {
         assertEquals(Calculator.Add("1,2"),3);
     }
 
     @Test
-    public void returnsSumOfTwoNumbersNewLineSeperated(){
+    public void returnsSumOfTwoNumbersNewLineSeperated() throws Exception {
         assertEquals(Calculator.Add("1\n2"),3);
     }
 
     @Test
-    public void returnsSumOfMultipleNumbersWhichHasCustomDelimiterSeperated(){
+    public void returnsSumOfMultipleNumbersWhichHasCustomDelimiterSeperated() throws Exception {
         assertEquals(Calculator.Add("//;\n1;2"),3);
     }
 
+
     @Test
-    public void ignoreIfNumberIsGreaterThan1000(){
+    public void ignoreIfNumberIsGreaterThan1000() throws Exception {
         assertEquals(Calculator.Add("2,1001"),2);
     }
-
 
 }
